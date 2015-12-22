5118a4e2b5b890b2d7dc18fd8bcb40c96ce94f04
Anastassija <A.Izmaylova@cwi.nl>
2015-12-22 06:33:26 +0100

    Working on iggy ide: better handling of parse errors.

--- src/iggy/gen/parser/IGGYParser.java
+++ src/iggy/gen/parser/IGGYParser.java
@@ -6,8 +6,10 @@ import com.intellij.lang.ASTFactory;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiParser;
+import com.intellij.lang.impl.PsiBuilderImpl;
 import com.intellij.psi.impl.source.tree.CompositeElement;
 import com.intellij.psi.impl.source.tree.Factory;
+import com.intellij.psi.impl.source.tree.TreeElement;
 import com.intellij.psi.tree.IElementType;
 import iggy.gen.psi.IGGYElementTypes;
 import iggy.gen.psi.IGGYTokenTypes;
@@ -27,23 +29,31 @@ import org.iguana.parser.ParseResult;
 import org.iguana.traversal.idea.Names;
 import org.iguana.util.Configuration;
 
+import java.io.IOException;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IGGYParser implements PsiParser {
 
     private Grammar grammar;
     private Start start;
     private GrammarGraph graph;
+    private Pattern pattern = java.util.regex.Pattern.compile("([\\u0009-\\u000A\\u000C-\\u000D\\u0020-\\u0020]+|)([\\$-\\$A-Z_-_a-z]([\\$-\\$0-9A-Z_-_a-z]|[\\$-\\$A-Z_-_a-z])*)([\\u0009-\\u000A\\u000C-\\u000D\\u0020-\\u0020]+|)::=");
+
+    private static final String ntName = "Definition";
 
     public ASTNode parse(IElementType root, PsiBuilder builder) {
         Input input = Input.fromString(builder.getOriginalText().toString());
         if (graph == null) {
-            grammar = Grammar.load(this.getClass().getClassLoader().getResourceAsStream("iggy/gen/parser/grammar/IGGY"));
+            grammar = Grammar.load(this.getClass().getResourceAsStream("/IggyGrammar"));
             DesugarPrecedenceAndAssociativity precedenceAndAssociativity = new DesugarPrecedenceAndAssociativity();
             precedenceAndAssociativity.setOP2();
             grammar = new EBNFToBNF().transform(grammar);
             grammar = precedenceAndAssociativity.transform(grammar);
             grammar = new Names().transform(grammar);
             grammar = new LayoutWeaver().transform(grammar);
-            start = grammar.getStartSymbol(Nonterminal.withName("Definition"));
+            start = grammar.getStartSymbol(Nonterminal.withName(ntName));
             graph = GrammarGraph.from(grammar, input, Configuration.DEFAULT);
         }
         ParseResult result = Iguana.parse(input, graph, start);
@@ -60,11 +70,43 @@ public class IGGYParser implements PsiParser {
                 NonterminalNode sppf = graph.getHead(Nonterminal.withName("Rule+"))
                         .getGSSNode(l.getRightExtent()).getNonterminalNode(input);
                 if (sppf != null) {
-                    CompositeElement startNode = ASTFactory.composite(IGGYElementTypes.get("START_DEFINITION_START"));
+                    Matcher matcher = pattern.matcher(input.toString());
+                    int index = -1;
+                    ParseResult res = null;
+                    if (matcher.find(sppf.getRightExtent())) {
+                        index = matcher.start();
+                        res = Iguana.parse(Input.fromString(input.subString(index, input.length())), graph, start);
+                    }
+                    CompositeElement startNode = ASTFactory.composite(IGGYElementTypes.get("START_"+ ntName.toUpperCase() + "_START"));
                     startNode.rawAddChildrenWithoutNotifications(SPPFToTerms.convertNoSharing(l, new IGGYTermBuilder()));
-                    CompositeElement node = ASTFactory.composite(IGGYElementTypes.get("DEFINITION"));
+                    CompositeElement node = ASTFactory.composite(IGGYElementTypes.get(ntName.toUpperCase()));
                     CompositeElement child = (CompositeElement) SPPFToTerms.convertNoSharing(sppf, new IGGYTermBuilder());
                     CompositeElement error = Factory.createErrorElement("Sorry, you have a parse error.");
+                    if (res != null && res.isParseSuccess()) {
+                        child.rawAddChildrenWithoutNotifications(error);
+                        error.rawAddChildrenWithoutNotifications(ASTFactory.leaf(IGGYTokenTypes.TERMINAL, input.subString(sppf.getRightExtent(), index)));
+                        sppf = res.asParseSuccess().getSPPFNode();
+                        TreeElement ast = SPPFToTerms.convertNoSharing(sppf, new IGGYTermBuilder());
+                        TreeElement first = ast.getFirstChildNode();
+                        ast.getFirstChildNode().rawRemove();
+                        child.rawAddChildrenWithoutNotifications(first);
+                        first = ast.getFirstChildNode();
+                        ast.getFirstChildNode().rawRemove();
+                        TreeElement list = first.getFirstChildNode();
+                        first.getFirstChildNode().rawRemove();
+                        first = list.getFirstChildNode();
+                        while (first != null) {
+                            list.getFirstChildNode().rawRemove();
+                            child.rawAddChildrenWithoutNotifications(first);
+                            first = list.getFirstChildNode();
+                        }
+                        node.rawAddChildrenWithoutNotifications(child);
+                        startNode.rawAddChildrenWithoutNotifications(node);
+                        first = ast.getFirstChildNode();
+                        ast.getFirstChildNode().rawRemove();
+                        startNode.rawAddChildrenWithoutNotifications(first);
+                        return startNode;
+                    }
                     node.rawAddChildrenWithoutNotifications(child);
                     startNode.rawAddChildrenWithoutNotifications(node);
                     startNode.rawAddChildrenWithoutNotifications(error);
